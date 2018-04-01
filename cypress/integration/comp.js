5bb3d9b33a8e985764e5ed4f45dfa0e917856937
eb <EB>
2018-04-01 22:03:22 +0200

    Tweak wording

--- cypress/integration/comp.js
+++ cypress/integration/comp.js
@@ -2,8 +2,8 @@ import {mount} from 'cypress-react-unit-test';
 import Select from '../../components/Document';
 import React from 'react';
 
-describe('a document', () => {
-  it('should stub XHR network request, retrieving a document and rendering it', () => {
+describe('translation workbench', () => {
+  it('should stub a document using a fixture and display it', () => {
 
     // fake server configuration
     cy.server({method: 'GET', delay: 3000, status: 200, response: {}});
@@ -14,9 +14,9 @@ describe('a document', () => {
     mount(<Select/>);
   });
 
-  it('should take user input', () => {
+  it('should accept user input', () => {
     cy.get('#1').type('Gjør det enkleste som muligens kan fungere.');
     cy.get('#2').type('Dilemmaet med eldre kodebaser: Før vi endrer noe kode, bør koden dekkes av tester. Men, for å få på plass tester, er det ofte nødvendig å gjøre endringer i koden.');
-    cy.get('#3').type('XP-filosofien går ut på å begynne der du er nå og jobbe mot idealet. Mao, er det noe du kan gjøre for å forbedre deg i forhold til der du befinner deg nå?');
+    cy.get('#3').type('XP-filosofien går ut på å begynne der du er nå og jobbe mot idealet. Mao., finnes det noen små grep du kan ta for å forbedre ferdighetene dine?');
   })
 });
